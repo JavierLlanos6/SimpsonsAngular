d9a571cc399980d3deb8b5b6ce374a39591b1e85
JavierLlanos6 <xavi6poop@gmail.com>
2026-04-23 14:09:07 -0400

    Project added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Videojuegos } from './components/videojuegos/videojuegos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Videojuegos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('practica-angular-21');
+  descripcion = 'prueba de angular para aprender'
 }
