889d3ae7b80a2a8aada4eaa636c12cca1cf8cef2
JavierLlanos6 <xavi6poop@gmail.com>
2026-04-23 14:36:51 -0400

    redirect added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'characters',
+        loadChildren: () =>
+            import('./features/characters/characters-module').then(
+                (m) => m.CharactersModule
+            ),
+    },
+    { path: '', redirectTo: 'characters', pathMatch: 'full' }
+];
