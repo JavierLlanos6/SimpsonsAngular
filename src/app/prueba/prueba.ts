0eb6a29a39dc10189654307854cb2c54022e39c8
“JavierLlanos6” <xavi6poop@gmail.com>
2026-04-27 09:26:57 -0400

    Ajustado con validaciones en el formulario

--- src/app/prueba/prueba.ts
+++ src/app/prueba/prueba.ts
@@ -4,22 +4,23 @@ import { ApiService } from "../features/characters/services/api/api";
 import { RouterLink } from "@angular/router";
 import { Product } from "./products/product.model";
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
+import { createProductForm } from "./forms/prodducto-forms";
 
 @Component({
-  standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule],
-  templateUrl: './prueba.html',
+    standalone: true,
+    imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
+    templateUrl: './prueba.html',
 })
 export class PruebaCrudClass implements OnInit {
-
     editingProductId: number | null = null;
-
     products: Product[] = [];
     loading = true;
 
     constructor(
         private api: ApiService,
-        private cdr: ChangeDetectorRef
+        private cdr: ChangeDetectorRef,
+        private fb: FormBuilder
     ) {}
 
     showForm = false;
@@ -31,55 +32,48 @@ export class PruebaCrudClass implements OnInit {
         image: '' 
     };
 
+    //validaciones:
+    productForm!: any;
+    ngOnInit(): void {
+    this.initForm();
+    this.getProducts();
+    }
+
+    initForm() {
+        this.productForm = createProductForm(this.fb);
+    }
+
     toggleForm() {
         this.showForm = !this.showForm;
     }
 
-    /*addLocalProduct() {
-        const product: Product = {
-            id: Date.now(),
-            ...this.newProduct,
-            image: ''
-        };
-
-        console.log('[LOCAL CREATE]:', product);
-
-        // guardar en memoria
-        this.products.unshift(product);
-
-        // guardar en localStorage
-        const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
-        localProducts.unshift(product);
-
-        localStorage.setItem('products', JSON.stringify(localProducts));
-
-        // reset
-        this.newProduct = {
-            title: '',
-            price: 0,
-            description: '',
-            category: '',
-            image: ''
-        };
-
-        this.showForm = false;
-    }*/
-
     saveProduct() {
+        if (this.productForm.invalid) {
+            this.productForm.markAllAsTouched();
+            return;
+        }
+
+        const formValue = this.productForm.value;
+
+        const safeProduct = {
+            title: formValue.title ?? '',
+            price: formValue.price ?? 0,
+            description: formValue.description ?? '',
+            category: formValue.category ?? '',
+        };
+
         if (this.editingProductId) {
+            // EDITAR
             const updatedProduct: Product = {
-            id: this.editingProductId,
-            ...this.newProduct
+                id: this.editingProductId,
+                ...safeProduct,
+                image: ''
             };
 
-            console.log('CREANDO DE MANERA LOCAL:', updatedProduct);
-
-            // actualizar lista
             this.products = this.products.map(p =>
-            p.id === this.editingProductId ? updatedProduct : p
+                p.id === this.editingProductId ? updatedProduct : p
             );
 
-            // actualizar localStorage
             const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
             const updatedLocal = localProducts.map((p: Product) =>
             p.id === this.editingProductId ? updatedProduct : p
@@ -90,35 +84,22 @@ export class PruebaCrudClass implements OnInit {
             this.editingProductId = null;
 
         } else {
-            const product: Product = {
-            id: Date.now(),
-            ...this.newProduct,
-            image: ''
+            // CREAR
+            const newProduct: Product = {
+                id: Date.now(),
+                ...safeProduct,
+                image: ''
             };
 
-            console.log('CREANDO local:', product);
-
-            this.products.unshift(product);
+            this.products.unshift(newProduct);
 
             const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
-            localProducts.unshift(product);
+            localProducts.unshift(newProduct);
             localStorage.setItem('products', JSON.stringify(localProducts));
         }
 
-        this.newProduct = {
-            title: '',
-            price: 0,
-            description: '',
-            category: '',
-            image: ''
-        };
-
+        this.productForm.reset();
         this.showForm = false;
-    }
-
-
-    ngOnInit(): void {
-        this.getProducts();
     }
 
     getProducts() {
@@ -126,32 +107,28 @@ export class PruebaCrudClass implements OnInit {
 
         this.api.getProducts().subscribe({
             next: (data: Product[]) => {
-
             // obtener localStorage
-            const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
+                const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
 
-            // combinar local con api
-            this.products = [...localProducts, ...data];
+                // combinar local con api
+                this.products = [...localProducts, ...data];
 
-            this.loading = false;
-            this.cdr.detectChanges();
-            },
-            error: (err) => {
-            console.error('ERROR:', err);
-            this.loading = false;
-            this.cdr.detectChanges();
+                this.loading = false;
+                this.cdr.detectChanges();
+                },
+                error: (err) => {
+                console.error('ERROR:', err);
+                this.loading = false;
+                this.cdr.detectChanges();
             }
         });
     }
 
     deleteProduct(id: number) {
         this.products = this.products.filter(p => p.id !== id);
-
         const localProducts = JSON.parse(localStorage.getItem('products') || '[]');
         const updated = localProducts.filter((p: Product) => p.id !== id);
-
         localStorage.setItem('products', JSON.stringify(updated));
-
         console.log('Eliminado localmente:', id);
     }
 
@@ -162,35 +139,14 @@ export class PruebaCrudClass implements OnInit {
         });
     }
 
-    /**editProduct(product: Product) {
-        console.log('Producto original:', product);
-
-        const updated = {
-            ...product,
-            title: product.title + ' AYUDAAAAAAAAAA'
-        };
-
-        this.api.updateProduct(product.id, updated).subscribe({
-            next: (res) => {
-            console.log('Producto ACTUALIZADO correctamente:', res);
-            this.getProducts();
-            },
-            error: (err) => {
-            console.error('ERROOOOR al querre actualizar:', err);
-            }
-        });
-    }*/
-   editProduct(product: Product) {
+    editProduct(product: Product) {
         this.editingProductId = product.id;
-
-        this.newProduct = {
+        this.productForm.patchValue({
             title: product.title,
             price: product.price,
             description: product.description,
-            category: product.category,
-            image: product.image
-        };
-
+            category: product.category
+        });
         this.showForm = true;
     }
 
