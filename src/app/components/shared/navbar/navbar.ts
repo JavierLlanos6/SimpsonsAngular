b4f7d25654be9d5eaf867a970f099e05c38f0e73
“JavierLlanos6” <xavi6poop@gmail.com>
2026-04-27 10:29:38 -0400

    Estilos para visualizar en que parte estoy en el navbar

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive,  } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   standalone: true
 })
